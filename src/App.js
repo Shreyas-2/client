9b74fcfb05a39602e341ae5f858dbeb2632b816b
Shreyas <shreyasanand619@gmail.com>
2023-10-17 21:30:57 +0530

    HOMEPAGE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+      <img src="/images/logo.png" alt="Company Logo" className="logo" />
+
+
       </header>
+      <div className="content">
+        <div className="left-content">
+          <h1>For Doctors</h1>
+          <a href="/login-doctor"><button>Login</button></a>
+          <p>Don't have an account? <a href="/register-doctor">Register</a></p>
+        </div>
+        <div className="right-content">
+          <h1>For Patients</h1>
+          <a href="/login-patient"><button>Login</button></a>
+          <p>Don't have an account?<a href="/register-patient">Register</a></p>
+        </div>
+      </div>
     </div>
   );
 }
